235c3cd3149d91449db0d4632d924150e42fa593
kf53495 <fkmndk902902@gmail.com>
2022-06-07 11:35:06 +0900

    flutter_hooksとhooks_riverpodsのセットアップ

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
-  runApp(
-    ProviderScope(
-      child: MyApp(),
-    ),
-  );
+  runApp(ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
