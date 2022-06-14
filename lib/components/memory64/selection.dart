3a97622ff7055b1c96b7e8eefe9afc793544bc2a
kf53495 <fkmndk902902@gmail.com>
2022-06-14 21:07:16 +0900

    ドロップダウンボックスで盤のサイズを入力するフォームを作成

--- lib/components/memory64/selection.dart
+++ lib/components/memory64/selection.dart
@@ -1,19 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import '../global_components/appbar.dart';
+import 'providers/board_size.dart';
 import 'quiz.dart';
 
-class Memory64Selection extends StatelessWidget {
+class Memory64Selection extends ConsumerWidget {
   const Memory64Selection({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: GlobalAppBar(),
       body: Column(
         children: [
           Container(
             width: 200,
-            child: TextField(),
+            child: DropdownButton(
+              items: [
+                for (int i = 1; i <= 5; i++)
+                  DropdownMenuItem(
+                    value: i * 4,
+                    child: Text('${i + 4} × ${i + 4}'),
+                  )
+              ],
+              onChanged: (int? value) {
+                if (value != null) {
+                  ref.watch(boardSizeProvider.notifier).state = value;
+                }
+              },
+            ),
           ),
           const SizedBox(
             width: 100,
@@ -29,7 +44,7 @@ class Memory64Selection extends StatelessWidget {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => Memory64Quiz(boardSize: 'ok'),
+                    builder: (context) => Memory64Quiz(boardSize: 16),
                   ),
                 );
               },
