13fb6d06b00078dfbb07bca6ff157e2fac0a2686
kf53495 <fkmndk902902@gmail.com>
2022-06-16 18:51:43 +0900

    timeProviderをStateNotifierProviderに変更

--- lib/components/memory64/providers/time_provider.dart
+++ lib/components/memory64/providers/time_provider.dart
@@ -1,3 +1,16 @@
+import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-final timeProvider = StateProvider<int>((ref) => 0);
+@immutable
+class Time {
+  const Time({required this.time});
+  final int time;
+}
+
+class TimeNotifier extends StateNotifier<int> {
+  TimeNotifier() : super(0);
+}
+
+final timeProvider = StateNotifierProvider<TimeNotifier, int>(
+  (ref) => TimeNotifier(),
+);
