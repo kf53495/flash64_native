5e69efad8c3c355923d87ce4ca9dcd0c5175b029
kf53495 <fkmndk902902@gmail.com>
2022-06-17 18:10:06 +0900

    Revert "timeProviderをStateNotifierProviderに変更"
    This reverts commit 13fb6d06b00078dfbb07bca6ff157e2fac0a2686.

--- lib/components/memory64/providers/time_provider.dart
+++ lib/components/memory64/providers/time_provider.dart
@@ -1,16 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-@immutable
-class Time {
-  const Time({required this.time});
-  final int time;
-}
-
-class TimeNotifier extends StateNotifier<int> {
-  TimeNotifier() : super(0);
-}
-
-final timeProvider = StateNotifierProvider<TimeNotifier, int>(
-  (ref) => TimeNotifier(),
-);
+final timeProvider = StateProvider<int>((ref) => 0);
