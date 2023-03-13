ea94eb03fa9c7778f4900542ed945babf4e5002b
kf53495 <fkmndk902902@gmail.com>
2023-03-13 16:57:21 +0900

    mental calcのクイズページの基礎を作成

---
+++ lib/components/mental_calc/quiz.dart
@@ -0,0 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flash64_native/components/select_quiz.dart';
+import 'package:flash64_native/components/users/login.dart';
+import 'package:flash64_native/components/users/my_page.dart';
+import 'package:flash64_native/components/users/providers/user_info.dart';
+import 'package:flash64_native/components/users/registration.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import '../global_components/appbar.dart';
+
+class MentalCalcQuiz extends ConsumerWidget {
+  const MentalCalcQuiz({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold();
+  }
+}
